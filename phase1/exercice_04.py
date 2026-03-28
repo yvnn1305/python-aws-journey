8f41ee8f03981da8893efa61932c509a6904c393
Yann Kombo-larra <yannced1305@gamil.com>
2026-03-28 19:58:52 +0100

    add exercice 04 - write csv

---
+++ phase1/exercice_04.py
@@ -0,0 +1,28 @@
+import csv
+
+def word_file(filename):
+    with open(filename, 'r') as f:
+        content = f.read()
+        word_list = content.split()
+        result = {}
+        for mot in word_list:
+            if mot in result:
+                result[mot] += 1
+            else:
+                result[mot] = 1
+        return result
+
+def write_csv(filename):
+    tab = []
+    data = word_file(filename)
+    for mot in data:
+        tab.append({'mot': mot, 'occurences': data[mot]})
+    print(tab)
+
+    with open('fichier_python.csv', 'w', newline='') as csvfile:
+        fieldnames = ['mot', 'occurences']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(tab)
+
+write_csv("mots.txt")
